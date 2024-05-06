a916a75de7a325053cefcb2da9aa406f94fdae43
Yeani <ifeanyi200019@gmail.com>
2024-05-06 16:50:27 -0400

    have the adress form rendering on cart

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,14 +1,17 @@
 import { Tabs } from "../../components/Tabs";
 import Button from "../../components/elements/Button";
 import { useSelector } from "react-redux";
-import { selectAllProducts } from "../../stores/menu/productsSlice"
-
+import { cartProducts } from "../../stores/cart/cartSlice";
+import useTabSwitch from "../../hooks/useTabSwitch";
+import { ReactComponent as ArrowRightSvg } from "../../assets/icons/right-arrow-svgrepo-com.svg";
+import { AddressForm } from "../../components/AddressForm"
 
 const Cart = () => {
-    const cart = useSelector(selectAllProducts);
+    const cart = useSelector(cartProducts);
     const tabs= ['Summary', 'Delivery', 'Payment'];
+    const [currentTab, handleTabSwitch] = useTabSwitch(tabs, 'Summary');
 
-    if (!cart || cart.products.length === 0) {
+    if (!cart || cart.length === 0) {
         return (
             <div className="bg-white h-full text-black flex justify-center p-4">
                 <h1>Your Cart is empty</h1>
@@ -17,9 +20,19 @@ const Cart = () => {
     }
 
     return (
-        <>Cart</>
-    )
-    
+        <div className="bg-white h-screen text-black mx-auto mt-2 border border-gray-200 p-4 md:w-2/3 rounded-lg shadow-md sm:p-6 lg:p-8">
+            <Tabs list={tabs} onTabSwitch={handleTabSwitch} activeTab={currentTab} />
+            <div className={`tabs ${currentTab !== 'Summary' ? 'hidden' : ''}`}>
+                Summary
+            </div>
+            <div className={`tabs ${currentTab !== 'Delivery' ? 'hidden' : ''}`}>
+                <AddressForm />
+            </div>
+            <div className={`tabs ${currentTab !== 'Payment' ? 'hidden' : ''}`}>
+                Payment Form
+            </div>
+        </div>
+    ) 
 }
 
 export default Cart;
